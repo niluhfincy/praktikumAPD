4feb479cd8438b6338b630a92ee2b6bb8b5e2da2
niluhfincy <niluhfincyglorianathasia@gmail.com>
2024-09-18 19:57:35 +0800

    Add files via upload

---
+++ praktikum-APD/Post-Test/kelas A1 2024/Pertemuan 3/pertemuan3.py
@@ -0,0 +1,8 @@
+masukkan = int(input("Masukkan angka 1: "))
+masukkan = int(input("Masukkan angka 2: "))
+if 1>2 :
+   print("kelinci")
+elif 1<2 :
+   print("kura - kura") 
+else  : 
+   print("kerbau")
