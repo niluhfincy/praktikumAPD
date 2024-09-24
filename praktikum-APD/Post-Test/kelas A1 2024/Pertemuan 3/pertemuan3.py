97bc1ed69b33fbb4ae0f72f7fad1eb6fb92e0372
niluhfincy <niluhfincyglorianathasia@gmail.com>
2024-09-24 13:56:02 +0800

    Delete praktikum-APD/Post-Test/kelas A1 2024/Pertemuan 3 directory

--- praktikum-APD/Post-Test/kelas A1 2024/Pertemuan 3/pertemuan3.py
+++
@@ -1,8 +0,0 @@
-masukkan = int(input("Masukkan angka 1: "))
-masukkan = int(input("Masukkan angka 2: "))
-if 1>2 :
-   print("kelinci")
-elif 1<2 :
-   print("kura - kura") 
-else  : 
-   print("kerbau")
