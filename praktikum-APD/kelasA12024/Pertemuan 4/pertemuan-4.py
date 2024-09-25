8aca2f964dd0043e2565351dd61e4b61f295fb07
niluhfincy <niluhfincyglorianathasia@gmail.com>
2024-09-25 09:23:44 +0800

    Add files via upload

---
+++ praktikum-APD/kelasA12024/Pertemuan 4/pertemuan-4.py
@@ -0,0 +1,41 @@
+# ulang = 10
+# for i in range(ulang):
+#     print("Perulangan ke-" + str(i))
+
+
+# -
+# for i in range(1, 10, 2): #(awal, akhir, lompatan)
+#     # print(i)
+
+#     for i in range(1, 4):
+#         for j in range(1, 4):
+#           print(f"{i} x {j} = {i * j}")
+# print()
+
+# hitung = 0
+# while True:
+#         hitung += 1
+#         ulang = input("Masih Ingin Mengulang? ")
+#         if ulang == "tidak" or ulang =="Tidak":
+#             break
+#print(f"Total Perulangan: {hitung}")
+
+hitung = 0
+
+# while True: # selalu benar 
+
+#     print(" Daftar bilangan ")
+#     for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+
+#bilangan = 0
+# while True:
+#    angka = int(input("masukkan angka: "))
+    # if angka < 0:
+    #     break
+    # bilangan += angka
+
+
