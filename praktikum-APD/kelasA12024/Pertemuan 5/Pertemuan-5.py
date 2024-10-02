46c49f83e18ec6b5c7aadbfc367c706bbed5bf2a
niluhfincy <niluhfincyglorianathasia@gmail.com>
2024-10-02 12:07:32 +0800

    Add files via upload

---
+++ praktikum-APD/kelasA12024/Pertemuan 5/Pertemuan-5.py
@@ -0,0 +1,84 @@
+#nama = ["celio", 
+#        "shandy", 
+#         "farel",
+#       "ghazali", 
+#        "vito", 
+#        "yuyun", 
+#       "adri", 
+#         "rizal", 
+#         "adi", 
+#         "ifnu"]
+
+
+#matkul = ["APD", "APL", "BASDAT", "STRUKDAT", "WEB", "JARKOM"]
+
+#data = nama+matkul
+
+#print("sebelum")    
+#print(nama)
+#print(" ")
+#print("sesudah:")
+
+#print.insert(2."afrizal")
+#print(nama)
+#nama[4]= "fufufafa"
+#hapus = nama.pop(2)
+#print(nama)
+#print(hapus)
+
+#print(nama[1:9:2])
+
+#data = ["farel", "celio", [1,2, ["hallo",23,2,3,True]]]
+
+#print(data[2][2][0][::-1])
+
+#print(data[::-1])
+
+#matkul = [1,2,3],[4,5,6,],[7,8,9]
+#print(matkul[4][2][::-1])
+
+#for i in matkul:
+#   for j in i:
+#       print(j)
+
+#nama("farel", "vito", " sandy", "rijal")
+#print(nama[1:])
+
+#mahasiswa = (69, "informatika", "2209106044", "Aldy septian")
+#absen, prodi, nim, nama = matkul
+#print(nim)
+
+
+
+#Group
+#print(
+#"""
+#=============================
+#|    DATA MAHASISWA         |
+# =============================
+# |    1. TAMBAH DATA         |
+# |    2. TAMPILAN DATA       |
+# |    3. UBAH DATA           |
+# |    4. HAPUS DATA          |
+# |    5. KELUAR              |
+# =============================
+# """
+    #     nama_lama = input("Nama Baru : ")
+    #     for i in range(len(data_mahasiswa)):
+    #         if data_mahasiswa[i][0] == nama_lama:
+    #             nama_baru = input("NAMA : ")
+    #             nim_baru = input("NIM : ")
+    #             kelas_baru = input("KELAS : ")
+    #             data_mahasiswa[i][0] = nama_baru
+    #             data_mahasiswa[i][1] = nim_baru
+    #             data_mahasiswa[i][2] = kelas_baru
+    # elif pilih == 4:
+    #     nama_lama = input("Nama yang ingin dihapus")
+    #     for i in range(len(data_mahasiswa)):
+    #         if data_mahasiswa[i][0] == nama_lama:
+    #             del data_mahasiswa[i]
+    # elif pilih == 5:
+    #     print("Terima Kasih Telah Mengakses Data Mahasiswa")
+    #     break
+    # else:
+    #     print("Anda Salah Input")
